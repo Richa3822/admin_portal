b0be82bf1b86c294c35ab7536a6adb7a8570aa8f
Rahul Sharma <rahul@cloudmatetechnologies.com>
2023-04-12 18:20:32 +0530

    bugfix: Url fix

--- src/constants/Constant.jsx
+++ src/constants/Constant.jsx
@@ -1,6 +1,8 @@
+import { API_BASE_URL } from "../config";
+
 export const WAITING_TIMING = 500
 export const LIMIT = 10
 export const PAGES_COUNT = 3
-export const ORDER_URL = "http://localhost:4000/api/order/admin";
-export const ORDER_BY_ID_URL = "http://localhost:4000/api/order/admin";
-export const UPDATE_ORDER_URL = "http://localhost:4000/api/order";
+export const ORDER_URL = `${API_BASE_URL}order/admin`;
+export const ORDER_BY_ID_URL = `${API_BASE_URL}order/admin`;
+export const UPDATE_ORDER_URL = `${API_BASE_URL}order`;
